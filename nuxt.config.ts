555d5caf644d3bde70d814d918f9b390526794e9
島　浩文 <shima@strawhat.onmicrosoft.com>
2026-01-19 15:25:51 +0900

    直リンク対応

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,17 +11,6 @@ export default defineNuxtConfig({
     '~/assets/css/main.css',
   ],
 
-  nitro: {
-    prerender: {
-      crawlLinks: true, // サイト内のリンクをクロールして静的ファイルを生成
-      routes: ['/', '/step1', '/about', '/sweettrap']     // 生成したいページを明示的に指定
-    }
-  },
-
-  experimental: {
-    payloadExtraction: false
-  },
-
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
 })
